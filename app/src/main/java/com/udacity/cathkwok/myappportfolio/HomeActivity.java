bc8a53a30be90b960fc04d9a0c3e10855e6de6aa
Kwok <cathkwok@amazon.com>
2016-11-02 11:12:07 -0700

    Added stub buttons

--- app/src/main/java/com/udacity/cathkwok/myappportfolio/HomeActivity.java
+++ app/src/main/java/com/udacity/cathkwok/myappportfolio/HomeActivity.java
@@ -2,6 +2,9 @@ package com.udacity.cathkwok.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchApp(View view) {
+        if (view instanceof Button) {
+            Button button = (Button) view;
+            String toastMessage = getResources().getString(R.string.toast_message, button.getText().toString().toLowerCase());
+            Toast toast = Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
